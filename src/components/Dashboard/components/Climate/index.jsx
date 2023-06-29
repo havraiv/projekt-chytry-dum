3ee0f5cf3aab5b17037263e3d518e1131251a2ec
Iva Havranová <havranova.i@seznam.cz>
2023-06-29 21:51:37 +0200

    Climate component done

--- src/components/Dashboard/components/Climate/index.jsx
+++ src/components/Dashboard/components/Climate/index.jsx
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import tempImg from './img/temp.svg';
 
-export const Climate = () => {
+export const Climate = ({ temperature, humidity }) => {
+  const [temp, setTemp] = useState(temperature);
   return (
     <div className="climate">
       <div className="climate__icon">
         <img src={tempImg} />
       </div>
       <div className="climate__content">
-        <div className="climate__temperature">24&deg;C</div>
-        <div className="climate__humidity">Vlhost vzduchu 51&nbsp;%</div>
+        <div className="climate__temperature">{temp}&deg;C</div>
+        <div className="climate__humidity">
+          Vlhost vzduchu {humidity}&nbsp;%
+        </div>
       </div>
       <div className="climate__controls">
-        <button className="button">+</button>
-        <button className="button">-</button>
+        <button
+          className="button"
+          onClick={() => {
+            setTemp(temp + 1);
+          }}
+        >
+          +
+        </button>
+        <button
+          className="button"
+          onClick={() => {
+            setTemp(temp - 1);
+          }}
+        >
+          -
+        </button>
       </div>
     </div>
   );
--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './style.css';
 import { Lights } from './components/Lights';
+import { Climate } from './components/Climate';
 
 export const Dashboard = ({ data }) => {
   return (
     <main className="dashboard">
       <Lights lights={data.lights} />
+      <Climate
+        temperature={data.climate.temperature}
+        humidity={data.climate.humidity}
+      />
     </main>
   );
 };
